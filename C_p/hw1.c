d4ee4cc95c6692e38badf26d7cc9bac36120da5f
youseunghwan <129355719+yooseunghwa@users.noreply.github.com>
2024-09-12 16:43:53 +0900

    hw1

--- C_p/hw1.c
+++ C_p/hw1.c
@@ -2,13 +2,16 @@
 
 int main(void){
 
-    int num1=2, num2=4,num3=6;
+    int num1,num2;
 
-    int test=912;
+    printf("첫번째 정수를 입력하시오 :");
+    scanf("%d",&num1);
+    printf("두번째 정수를 입력하시오 :");
+    scanf("%d",&num2);
 
-    print("%d",test);
-
-    print("test2");
+    printf("%d & %d = %d\n",num1,num2,num1&num2);
+    printf("%d | %d = %d\n",num1,num2,num1|num2);
+    printf("%d ^ %d = %d\n",num1,num2,num1^num2);
     
 
     return 0;
