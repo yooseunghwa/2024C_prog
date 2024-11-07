3f7cc13e768c170374b643d7b3de0166ff4ffe49
youseunghwan <129355719+yooseunghwa@users.noreply.github.com>
2024-11-07 16:25:59 +0900

    'other''

--- C_p/hw7.c
+++ C_p/hw7.c
@@ -1,40 +1,25 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main(void){
     int arr1[6]={1,2,3,4,5,6};
     int arr2[6]={7,8,9,10,11,12};
+    
+    int *ptr1 = arr1;
+    int *ptr2 = arr2;
     int temp,i;
-    int* ptr1 = arr1;
-    int* ptr2 = arr2;
 
-    printf("arr1 : ");
-    for (i=0;i<6;i++){
-        printf("%d ",arr1[i]);
-    }
-    printf("\n");
-    printf("arr2 : ");
-    for (i=0;i<6;i++){
-        printf("%d ",arr2[i]);
-    }
-    printf("\n");
-
+    printf("arr1 : %d %d %d %d %d %d\n",arr1[0],arr1[1],arr1[2],arr1[3],arr1[4],arr1[5]);
+    printf("arr2 : %d %d %d %d %d %d\n",arr2[0],arr2[1],arr2[2],arr2[3],arr2[4],arr2[5]);
 
     for (i=0;i<6;i++){
-        temp = ptr1[i];
-        ptr1[i] = ptr2[i];
-        ptr2[i] = temp;
+        temp=*ptr1;
+        *ptr1=*ptr2;
+        *ptr2=temp;
+        ptr1++;
+        ptr2++;
     }
-
     printf("after swap\n");
-    printf("arr1 : ");
-    for (i=0;i<6;i++){
-        printf("%d ",arr1[i]);
-    }
-    printf("\n");
-    printf("arr2 : ");
-    for (i=0;i<6;i++){
-        printf("%d ",arr2[i]);
-    }
 
+    printf("arr1 : %d %d %d %d %d %d\n",arr1[0],arr1[1],arr1[2],arr1[3],arr1[4],arr1[5]);
+    printf("arr2 : %d %d %d %d %d %d\n",arr2[0],arr2[1],arr2[2],arr2[3],arr2[4],arr2[5]);
     return 0;
 }
